f8f19000148ecff2cfd6a3f04ea42ee9f829cd48
brokenSensor <jonimen95@yandex.ru>
2021-02-15 09:33:30 +0800

    Clean up and some css

--- components/NavProfile.jsx
+++ components/NavProfile.jsx
@@ -1,13 +1,3 @@
-{
-	/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown> */
-}
-
 import {
 	Navbar,
 	Nav,
@@ -21,10 +11,6 @@ import {
 import { useRouter } from 'next/router';
 import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from '../styles/Navbar.module.css';
-
-// {!session && <>   Not signed in <br/>   <button onClick={() => signIn()}>Sign
-// in</button> </>} {session && <>   Signed in as {session.user.email} <br/>
-// <button onClick={() => signOut()}>Sign out</button> </>}
 
 function NavProfile() {
 	const [session, loading] = useSession();
@@ -64,10 +50,3 @@ function NavProfile() {
 }
 
 export default NavProfile;
-
-{
-	/* <Nav.Link onClick={() => router.push('/profile')}>
-            {session.user.name + " "}
-            <Image src={session.user.image} roundedCircle className={styles.profileImage}></Image>
-        </Nav.Link> */
-}
